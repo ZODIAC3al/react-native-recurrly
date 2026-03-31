16681feb6064003bb3c6499c9926a4b400b91d51
zodiac <alimaherr47@gmail.com>
2026-04-01 00:46:14 +0200

    adding font typography

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -9,24 +9,22 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className="flex-1 p-5 bg-background">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
+      <Text className="text-7xl font-sans-extrabold ">Home</Text>
       <Link
         href="/onboarding"
-        className="m-4 rounded bg-primary text-white p-4"
+        className="m-4 font-sans-bold rounded bg-primary text-white p-4"
       >
         Go to Onboarding{" "}
       </Link>
       <Link
         href="/(auth)/sign-in"
-        className="m-4 rounded bg-primary text-white p-4"
+        className="m-4 font-sans-bold rounded bg-primary text-white p-4"
       >
         Go to Sign In
       </Link>
       <Link
         href="/(auth)/sign-up"
-        className="m-4 rounded bg-primary text-white p-4"
+        className="m-4 font-sans-bold rounded bg-primary text-white p-4"
       >
         Go to Sign Up
       </Link>
